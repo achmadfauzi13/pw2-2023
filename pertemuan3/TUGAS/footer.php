44d7b4807a262a1f356afde477a7bfd58cd3f842
Fauzi_◉⁠ ͜⁠ ⁠ʖ <146054432+achmadfauzi13@users.noreply.github.com>
2024-03-20 15:47:27 +0700

    Delete pertemuan3 directory

--- pertemuan3/TUGAS/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- TAMPILKAN BAGIAN FOOTER DI SINI -->
-
-<footer class="text-center py-3 bg-light mt-5">
-        <p>Created by <a href="#">Achmad Fauzi</a> &copy; <?php echo date('Y') ?></p>
-    </footer>
-</body>
-
-</html>
--- pertemuan3/praktikum/libfungsi.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    function hitungluas($alas, $tinggi) {
-        $luas = 0.5 * $alas * $tinggi;
-        return $luas;
-    }
-
-?>
