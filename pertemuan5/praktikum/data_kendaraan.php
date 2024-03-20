7546db5775741d5fcb7dfedbc51fc17a4108e5bd
Fauzi_◉⁠ ͜⁠ ⁠ʖ <146054432+achmadfauzi13@users.noreply.github.com>
2024-03-20 15:19:32 +0700

    Add files via upload

---
+++ pertemuan5/praktikum/data_kendaraan.php
@@ -0,0 +1,16 @@
+<?php
+
+    require_once "class_kendaraan.php";
+
+    // Objek
+    $motor = new Motor('Motor','Bensin',2);
+    $submarine = new Submarine('Submarine','Solar',1000);
+
+    // echo
+    echo "Info Motor : <br>";
+    $motor->getInfoMotor();
+    echo "<br>";
+    echo "Info Submarine : <br>";
+    $submarine->getInfoSubmarine();
+
+?>
